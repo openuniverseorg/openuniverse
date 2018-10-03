b8c85b1538f0cabb323e2dd996437518ea021f73
Marcelo <jmfda00@gmail.com>
2018-10-03 14:02:20 -0300

    Adding new info to project show page

--- openuniverse/openuniverse/settings.py
+++ openuniverse/openuniverse/settings.py
@@ -61,8 +61,11 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
+            'libraries': {
+                'to_datetime':'openuniverse.templatetags.to_datetime'},
+            
         },
     },
 ]
