6792be240c5706ce88c4214eb71e6135098c8aa3
Felipe Fronchetti <fronchetti@usp.br>
2018-11-09 05:40:45 +0000

    Update Travis CI

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  cd public
+  git init
+
+  git remote add deploy openuniverse@206.189.177.194:/home/debian/continuousdeployment.git
+  git config user.name "Travis CI"
+  git config user.email "fronchetti@usp.br"
+
+  git add .
+  git commit -m "Deploy"
+  git push --force deploy master
+else
+  echo "Not deploying, since this branch isn't master."
+fi
