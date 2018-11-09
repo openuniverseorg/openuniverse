cc3f970bf5d9789286ca4d5a81907fdb4c9915ca
Felipe Fronchetti <fronchetti@usp.br>
2018-11-09 05:49:34 +0000

    Update Travis CI

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  cd public
-  git init
-
+  echo "Updating repository."
+  cd /home/debian/continuousdeployment.git
   git remote add deploy openuniverse@206.189.177.194:/home/debian/continuousdeployment.git
   git config user.name "Travis CI"
   git config user.email "fronchetti@usp.br"
