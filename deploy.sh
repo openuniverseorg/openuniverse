a5dec048a1a550459149d38969ba7c8d16669bb8
Felipe Fronchetti <fronchetti@usp.br>
2018-11-09 04:27:10 -0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   git config user.name "Travis CI"
   git config user.email "fronchetti@usp.br"
   git add .
-  git commit -m "Deploy"
+  git commit -m "[Travis CI] Continuous Deployment"
   git push --force deploy master
 else
   echo "[Travis CI] Not deploying, since this branch is not master."
