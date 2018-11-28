bfcef63ac5ca8ceaf05bdd7e9a3aaa6c78e1a039
Marcelo <jmfda00@gmail.com>
2018-11-28 11:35:57 -0300

    Add pagination to API

--- source/openuniverse/settings.py
+++ source/openuniverse/settings.py
@@ -161,5 +161,7 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
