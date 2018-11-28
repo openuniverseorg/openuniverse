23d343efa18a02c53b4e8cb335fbc8add035d327
Marcelo <jmfda00@gmail.com>
2018-11-28 09:48:24 -0300

    Add API root endpoint

--- source/api/views.py
+++ source/api/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from website.models import Project, ProjectStatistics, ProjectFeatures, TimeSeries
 from .serializers import ProjectSerializer, ProjectStatisticsSerializer, ProjectFeaturesSerializer, TimeSeriesSerializer
-
+#API root:
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 '''
 ModelList method return single instances of a certain model, a read-only endpoint.
 ModelDetail methods return single deatailed instances of a certain model, supporting CRUD API operations.
 
 '''
-# This variable sets read-only permission to not authenticated users to
-# all endpoints
+#API root:
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'project': reverse('project-list', request=request, format=format),
+        'project-statistics': reverse('project-statistics-list', request=request, format=format),
+        'project-features': reverse('project-features-list', request=request, format=format),
+        'time-series': reverse('time-series-list', request=request, format=format),
+    })
+
 
 
 class ProjectList(generics.ListCreateAPIView):
