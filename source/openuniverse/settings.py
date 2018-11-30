2cdb6cd3909b336fed1fab9ffb1ccce7cf769134
João Marcelo Freitas de Almeida <jmfda00@gmail.com>
2018-11-30 09:56:02 -0300

    Add throttling policy to API

--- source/openuniverse/settings.py
+++ source/openuniverse/settings.py
@@ -164,4 +164,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '200/day',
+        'user': '2000/day'
+    }
 }
